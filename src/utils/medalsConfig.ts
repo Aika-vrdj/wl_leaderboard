565718ff132e868642a49966ecedf316b32ea62f
Aika-vrdj <151849141+Aika-vrdj@users.noreply.github.com>
2025-02-02 21:45:53 -0500

    Update medalsConfig.ts
    added images from supabase bucket

--- src/utils/medalsConfig.ts
+++ src/utils/medalsConfig.ts
@@ -4,55 +4,55 @@ export const medalsConfig = [
     type: "type",
     value: "food",
     description: "Earned for collecting all food items.",
-    image: "/icons/hungry-hippo.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/1.png" // Add image path here
   },
   {
     name: "Over-prepared",
     type: "type",
     value: "gear",
     description: "Earned for collecting all gear items.",
-    image: "/icons/over-prepared.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/2.png" // Add image path here
   },
   {
     name: "Junk Collector",
     type: "type",
     value: "useless",
     description: "Earned for collecting all useless items.",
-    image: "/icons/junk-collector.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/3.png" // Add image path here
   },
   {
-    name: "Prize Sniper",
+    name: "Prize Hunter",
     type: "type",
     value: "cupon",
     description: "Earned for collecting all prizes.",
-    image: "/icons/prize-sniper.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/4.png" // Add image path here
   },
   {
     name: "Thrifty Looter",
     type: "rarity",
     value: "common",
     description: "Earned for collecting all common items.",
-    image: "/icons/thrifty-looter.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/5.png" // Add image path here
   },
   {
     name: "Picky Looter",
     type: "rarity",
     value: "uncommon",
     description: "Earned for collecting all uncommon items.",
-    image: "/icons/picky-looter.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/6.png" // Add image path here
   },
   {
     name: "Cultured Looter",
     type: "rarity",
     value: "rare",
     description: "Earned for collecting all rare items.",
-    image: "/icons/cultured-looter.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/7.png" // Add image path here
   },
   {
     name: "Legend Hunter",
     type: "rarity",
     value: "legendary",
     description: "Earned for collecting all legendary items.",
-    image: "/icons/legend-hunter.png" // Add image path here
+    image: "https://twyfuamtvdbtbnvbalbe.supabase.co/storage/v1/object/public/collectible-images/medals/8.png" // Add image path here
   }
 ];
