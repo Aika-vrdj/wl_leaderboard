65e9b055a47932eb69dec38bfd685dfd7875bbc6
Aika-vrdj <151849141+Aika-vrdj@users.noreply.github.com>
2025-02-02 21:31:55 -0500

    Update medalsConfig.ts

--- src/utils/medalsConfig.ts
+++ src/utils/medalsConfig.ts
@@ -3,48 +3,56 @@ export const medalsConfig = [
     name: "Hungry Hippo",
     type: "type",
     value: "food",
-    description: "Earned for collecting all food items."
+    description: "Earned for collecting all food items.",
+    image: "/icons/hungry-hippo.png" // Add image path here
   },
- {
+  {
     name: "Over-prepared",
     type: "type",
     value: "gear",
-    description: "Earned for collecting all gear items."
+    description: "Earned for collecting all gear items.",
+    image: "/icons/over-prepared.png" // Add image path here
   },
- {
+  {
     name: "Junk Collector",
     type: "type",
     value: "useless",
-    description: "Earned for collecting all useless items."
+    description: "Earned for collecting all useless items.",
+    image: "/icons/junk-collector.png" // Add image path here
   },
- {
-    name: "Prize Hunter",
+  {
+    name: "Prize Sniper",
     type: "type",
     value: "cupon",
-    description: "Earned for collecting all prizes."
+    description: "Earned for collecting all prizes.",
+    image: "/icons/prize-sniper.png" // Add image path here
   },
- {
+  {
     name: "Thrifty Looter",
     type: "rarity",
     value: "common",
-    description: "Earned for collecting all common items."
+    description: "Earned for collecting all common items.",
+    image: "/icons/thrifty-looter.png" // Add image path here
   },
   {
     name: "Picky Looter",
     type: "rarity",
     value: "uncommon",
-    description: "Earned for collecting all uncommon items."
+    description: "Earned for collecting all uncommon items.",
+    image: "/icons/picky-looter.png" // Add image path here
   },
   {
     name: "Cultured Looter",
     type: "rarity",
     value: "rare",
-    description: "Earned for collecting all rare items."
+    description: "Earned for collecting all rare items.",
+    image: "/icons/cultured-looter.png" // Add image path here
   },
   {
     name: "Legend Hunter",
     type: "rarity",
     value: "legendary",
-    description: "Earned for collecting all legendary items."
+    description: "Earned for collecting all legendary items.",
+    image: "/icons/legend-hunter.png" // Add image path here
   }
 ];
